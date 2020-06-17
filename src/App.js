2ffa2aa85d6403f46ecc317aeb8d186e06510f76
yuhao tan <tanyuhao@hotmail.com>
2020-06-17 15:49:34 +0800

    First commit of  Home Page with links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Home from "./Home"
 import Clock from './Clock';
+import {Switch, Route, Link} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Clock/>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/Clock">Clock</Link>
+      </nav>
+      <Switch>
+        <route exact path="/">
+          <Home/>
+        </route>
+        <route>
+          <Clock path="/clock"/>
+        </route>
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './styles.css'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+import App from './App';
 
 ReactDOM.render(
-  <App/>,  document.getElementById('root')
+  <Router>
+  <App/>
+  </Router>
+  ,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
